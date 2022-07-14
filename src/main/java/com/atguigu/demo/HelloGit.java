60c09f403877b54641819f095119a0f359e86c6b
szwww <2267681004@qq.com>
2022-07-14 19:28:15 +0800

    commit third

--- src/main/java/com/atguigu/demo/HelloGit.java
+++ src/main/java/com/atguigu/demo/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git version 1");
         System.out.println("hello git version 2");
+        System.out.println("hello git version 3");
     }
 }
